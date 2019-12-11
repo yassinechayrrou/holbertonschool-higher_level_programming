f00a0e84f8107f1697d711c1159b34eb8846c061
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-12-11 22:47:36 +0000

    red

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    convert = list(str)
+    for c in range(len(convert)):
+        if (ord(convert[i]) > 96 and ord(convert[i]) < 123):
+            convert[i] = chr(ord(convert[i]) - 32)
+    print("{}".format(("").join(convert)))
