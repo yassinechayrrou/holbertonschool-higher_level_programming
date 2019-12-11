4ed6b9010ed56bde748b20f3f2e56e2f7c357483
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-12-11 22:33:09 +0000

    test

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for n in range(9):
     for m in range(n + 1, 10):
-        print("{:d}{:d}, ".format(n, m), end="")
-print("{:d}{:d}".format(n + 1, m))
+        if n != 8 and m != 9:
+                print("{:d}{:d}, ".format(n, m), end="")
+print("{:d}{:d}".format(8, 9))
