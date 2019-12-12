1b0acf8711957cbfe7ccf307a59a43a968c60204
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-12-12 22:38:16 +0000

    fast

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    argc = len(argv)
-    i = 0
-    for j in range(1, argc):
-        i += int(argv[j])
-    print("{:d}".format(i))
+    import hidden_4
+    directory_arr = dir(hidden_4)
+    ignore = "__"
+    for i in range(0, len(directory_arr)):
+        if directory_arr[i] != ignore:
+            print(directory_arr[i])
