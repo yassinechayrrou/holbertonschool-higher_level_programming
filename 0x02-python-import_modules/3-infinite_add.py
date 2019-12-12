9bc51006443ea5a92e0296887accf6c8bacf18fb
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-12-12 22:07:15 +0000

    sum

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+        from sys import argv
+        argc = len(argv)
+        sum = 0
+        for count in range(1, argc):
+            sum =+ int(argv[count])
+        print(sum)
