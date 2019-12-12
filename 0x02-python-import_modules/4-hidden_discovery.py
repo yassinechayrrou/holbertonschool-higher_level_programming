f40ac27f077a3f647c0506a2728444a9666d8670
YassineChayrrou <55135383+YassineChayrrou@users.noreply.github.com>
2019-12-12 23:47:33 +0100

    Delete 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    argc = len(argv)
-    i = 0
-    for j in range(1, argc):
-        i += int(argv[j])
-    print("{:d}".format(i))
