901864456aa709010786a8d897b108d9286a2e48
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-12-12 22:42:48 +0000

    dd

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     directory_arr = dir(hidden_4)
     ignore = "__"
     for i in range(0, len(directory_arr)):
-        if directory_arr[i] != ignore:
+        if directory_arr[i][0] != ignore:
             print(directory_arr[i])
