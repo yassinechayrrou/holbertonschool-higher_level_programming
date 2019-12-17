6f5063c314109361ac2d82ae6f847ee74fac4ba8
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-12-17 22:15:41 +0000

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tup = (len(sentence), sentence[0])
+    tup = ([len(sentence), sentence[0]])
     if sentence:
         return tup
     else:
