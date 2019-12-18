9cac0c5d56cab897972427819923889dcb1d6b24
YassineChayrrou <yassine.chayrrou@gmail.com>
2019-12-18 22:43:03 +0000

    python simple calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":    
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    argc = len(argv) - 1
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    n = int(argv[1])
+    m = int(argv[3])
+    if argv[2] == "+":
+        print("{} + {} = {}".format(n, m, add(n, m)))
+    elif argv[2] == "-":
+        print("{} + {} = {}".format(n, m, sub(n, m)))
+    elif argv[2] == "*":
+        print("{} + {} = {}".format(n, m, mul(n, m)))
+    elif argv[2] == "/":
+        print("{} + {} = {}".format(n, m, div(n, m)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
