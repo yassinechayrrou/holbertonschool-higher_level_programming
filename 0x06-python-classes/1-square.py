86703bc2b3b9dc0b6f1d6137e92bcc372fea51b1
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-01-09 21:29:26 +0000

    task 1: square defined by size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""this is a square class it is defined by its attribute size"""
+class Square:
+    def __init__(self, size):
+        def __init__(self, size):
+            self.__size = size
