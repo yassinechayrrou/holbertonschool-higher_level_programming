004bd9283e718441488195fec5b5f1b06512664f
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-01-09 21:32:37 +0000

    fix duplicate def of class square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,5 +2,4 @@
 """this is a square class it is defined by its attribute size"""
 class Square:
     def __init__(self, size):
-        def __init__(self, size):
-            self.__size = size
+        self.__size = size
