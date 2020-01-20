7432fd77814c3eb1dbe74048bc1b800d9de0c3b5
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-01-20 21:13:18 +0000

    pep8 coding style fix

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,8 +11,7 @@ class Rectangle:
     methods: area, perimeter, print, str, repr, and del.
     """
     number_of_instances = 0
-    
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
