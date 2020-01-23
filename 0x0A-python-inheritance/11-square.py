1bd5890bc91ab76acb1870f94b4f4a2c57d76c67
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-01-23 21:55:51 +0000

    added integer validation with integer_validator

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,11 +6,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """inherits everything from rectangle 9 with super"""
     def __init__(self, size):
-        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        return self.__size * self.__size
+        self.integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
