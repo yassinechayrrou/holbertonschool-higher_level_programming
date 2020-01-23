7877cd2127ce9c5da9f9a668c52a1a298ed3f063
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-01-23 22:45:06 +0000

    fixed name self is not defined error

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,5 +5,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """inherits everything from rectangle 9 with super"""
-    def __init__(__self, size):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
