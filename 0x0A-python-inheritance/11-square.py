257ced4b3c4d91b661332ab0070df1e8bf381d3e
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-01-23 21:49:25 +0000

    added area attrib of square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,8 +6,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """inherits everything from rectangle 9 with super"""
     def __init__(self, size):
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
 
     def __str__(self):
         return "[Square] {:d}/{:d}".format(self.__size, self.__size)
