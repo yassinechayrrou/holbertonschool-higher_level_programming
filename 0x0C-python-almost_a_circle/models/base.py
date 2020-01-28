e5f3867f436d4dc8577a5ceedae0755a2a62e1af
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-01-28 21:39:12 +0000

    Second commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,7 +6,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is None:
+        if id == None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
