e0627c1b5e958ffa0ee004d80bd70baf0dc2ff76
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-19 21:38:59 +0000

    Task 6 combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 for n in range(10):
     for m in range(10):
-        if n != 8 and m != 9:
+        if n == 8 and m == 9:
+            pass
+        else:
             if n != m and n < m:
                 print("{:d}{:d}, ".format(n, m), end="")
 print("89")
