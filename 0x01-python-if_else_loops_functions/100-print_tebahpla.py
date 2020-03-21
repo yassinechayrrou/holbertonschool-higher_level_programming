d4e6438743d10d816b947a37b151742b963604da
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-21 17:32:06 +0000

    prints ASCII alphabets in reverse order alternating LC and UC

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-i = 90
-while i > 64:
-    if i % 2 == 0:
-        k = chr(i+32)
-    if i % 2 != 0:
-        k = chr(i)
-    print(k, end="")
-    i = i-1
+for i in 'zYxWvUtSrQpOnMlKjIhGfEdCbA':
+    print(i, end="")
