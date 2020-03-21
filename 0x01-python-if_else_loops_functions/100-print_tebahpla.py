42f1d117832e188f8a0b6637879448d7c5a3bd00
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-21 17:46:19 +0000

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 i = 90
 while i > 64:
-    print(chr(i) if i % 2 is not 0 else chr(i + 32), end="")
+    print("{:c}".format((i + 32) if not i % 2 else i), end="")
     i = i - 1
