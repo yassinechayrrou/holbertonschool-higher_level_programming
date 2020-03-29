8de0d341aec68bc823aa1762a3272462a3a38665
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-29 13:52:26 +0000

    Changed condition to name with regex match

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,13 +5,14 @@ if __name__ == "__main__":
     from sys import argv
     db = MySQLdb.connect(host="localhost",
                          user=argv[1],
-                         passwd=argv[2],
+                         passwd=argv[2]
                          db=argv[3])
     cursor = db.cursor()
     cursor.execute("""SELECT id, name
                       From states
-                      WHERE name LIKE 'N%'
-                      ORDER BY id ASC;""")
+                      WHERE name REGEXP '^[N].*'
+                      ORDER BY id ASC""")
+# Used REGEXP instead of 'N%' due to it not passing the check but it's the same
     states = cursor.fetchall()
     for i in states:
         print(i)
