1d5d44867c454798cf45198a39a415be1134ba8b
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-29 20:29:53 +0000

    Task 5: all cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""lists all cities in state"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    injection_check = MySQLdb.escape_string(argv[4]).decode()
+    cursor = db.cursor()
+    cursor.execute("""SELECT cities.name
+                      From states
+                      INNER JOIN cities ON states.id = cities.state_id
+                      WHERE states.name='{}'
+                      ORDER BY cities.id""".format(injection_check))
+    cities = cursor.fetchall()
+    for i in cities:
+        if i[0] is cities[len(cities) - 1][0]:
+            print(i[0])
+        else:
+            print("{}, ".format(i[0]), end="")
+    db.close()
