0e86b02162c756a07fb7e985a08a35cd5dd35afc
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-29 21:01:00 +0000

    fixed for output 6 and 8

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,18 +7,14 @@ if __name__ == "__main__":
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
-    injection_check = MySQLdb.escape_string(argv[4]).decode()
     cursor = db.cursor()
-    cursor.execute("""SELECT cities.name
-                      From states
-                      INNER JOIN cities ON states.id = cities.state_id
-                      WHERE states.name='{}'
-                      ORDER BY cities.id""".format(injection_check))
+    command = """SELECT cities.name From states
+                 INNER JOIN cities ON states.id = cities.state_id
+                 WHERE states.name = %s"""
+    cursor.execute(command, (argv[4], ))
     cities = cursor.fetchall()
-    for i in cities:
-        if i[0] is cities[len(cities) - 1][0]:
-            print(i[0])
-        else:
-            print("{}, ".format(i[0]), end="")
+    for i in range(len(cities) - 1):
+       print("{:s}, ".format(cities[i][0]), end="")
+    print(cities[len(cities) - 1][0])
     cursor.close()
     db.close()
