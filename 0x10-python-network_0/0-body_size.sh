6ea0a2c657fd5fdf83ea5a857aa7e951b027a6ff
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-04-12 20:21:04 +0000

    shebang written wrong

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # script that takes in a URL sends a request to that URL & displays the size of the body of the response
 curl -I "$1" | grep "Content-Length" | cut -d' ' -f2
