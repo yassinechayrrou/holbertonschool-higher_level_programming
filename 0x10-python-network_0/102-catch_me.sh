1134364997350b5d1f1a514c8dfaf87780cdad2c
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-04-12 22:09:59 +0000

    shebang error due to testing on container

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 # script that makes a request to server which causes a response containing specific value
-curl -s -L -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
