deae08b6370d6627b9ca2d91ca3a07eee845f2fa
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-04-12 21:15:00 +0000

    check delimeter

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -I "$1" | grep "Allow" | cut -d" " -f2-
+curl -s -I "$1" | grep "Allow" | cut -d":" -f2
