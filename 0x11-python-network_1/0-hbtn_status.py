004a7c764d4f171cc621fa31048b07a2a17fb8ec
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-04-16 19:21:10 +0000

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:\n\t- type: ", type(html))
+    print("\t- content: ", html)
+    print("\t- utf8 content: ", html.decode('utf-8'))
