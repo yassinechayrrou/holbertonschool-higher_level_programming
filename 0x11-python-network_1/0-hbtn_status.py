019244a5caf1e44f8378766b150e7cd3809cd44b
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-04-16 19:25:42 +0000

    code should not executed when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:\n\t- type: ", type(html))
-    print("\t- content: ", html)
-    print("\t- utf8 content: ", html.decode('utf-8'))
+"""python script for web page status test
+"""
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:\n\t- type: ", type(html))
+        print("\t- content: ", html)
+        print("\t- utf8 content: ", html.decode('utf-8'))
