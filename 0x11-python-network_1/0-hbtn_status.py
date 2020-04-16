1a62aaaf5b28767aa055b2e814b9737f7df61257
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-04-16 19:30:16 +0000

    addedd formatters

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print("Body response:\n\t- type: ", type(html))
-        print("\t- content: ", html)
-        print("\t- utf8 content: ", html.decode('utf-8'))
+        print("Body response:\n\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
