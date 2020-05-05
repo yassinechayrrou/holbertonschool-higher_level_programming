2b6056af1c200ae8783511eea9f8e238c87fc541
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-05 23:14:40 +0000

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const farg = process.argv[2];
-if (farg === undefined) {
+const farg = parseInt(process.argv[2]);
+if (farg === undefined || isNaN(farg)) {
   console.log('1');
 } else {
   let factorial = 1;
   let i;
-  for (i = parseInt(farg); i > 1; i--) {
+  for (i = farg; i > 1; i--) {
     factorial = factorial * i;
   }
   console.log(factorial);
