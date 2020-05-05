9edc0020f04e90ccd4909c79041c06d1dc0a929e
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-05 21:23:56 +0000

    Task 4: create sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv[2] === undefined) {
+  console.log('undefined is undefined');
+} else if (process.argv[3] === undefined) {
+  console.log(process.argv[2] + ' is undefined');
+} else {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+}
