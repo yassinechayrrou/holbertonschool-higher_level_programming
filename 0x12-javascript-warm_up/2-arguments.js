a2962eb3089bf4e9b28235b9674eeabe9797a1e5
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-05 20:41:24 +0000

    Task 2: Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const ac = process.argv.length;
-if (ac === 2) {
-  console.log('No arguement');
-} else if (ac === 3) {
-  console.log('Argument found');
+if (ac === 3) {
+  console.log('Arguement found');
+} else if (ac < 3) {
+  console.log('No argument');
 } else {
   console.log('Arguments found');
 }
