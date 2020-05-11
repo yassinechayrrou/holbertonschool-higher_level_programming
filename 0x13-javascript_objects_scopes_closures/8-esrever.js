0141ffb896578f21ca27303c248f120558c7bdbd
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-11 22:29:35 +0000

    Task 8: weird loop behaviour fixed

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let i;
-  const reverseList = [];
-  for (i = list.length - 1; i => 0; i--) {
-    reverseList.push(list[i]);
+  const reversedList = [];
+  for (i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
   }
-  return reverseList;
+  return reversedList;
 };
