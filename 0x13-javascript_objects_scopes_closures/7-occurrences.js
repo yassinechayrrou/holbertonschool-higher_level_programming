f58949053b9dc3f483c67480cae67d279166f8a6
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-11 22:10:23 +0000

    Task 7: occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  if (list === undefined) {
+    return 0;
+  }
+  let i;
+  let occ = 0;
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      occ++;
+    }
+  }
+  return occ;
+}
