c47514055513b494dae70bf14215969763fa7656
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-12 19:44:24 +0000

    Task 4: Star wars Wedge Antilles[i don't know him btw]

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const URL = 'https://swapi-api.hbtn.io/api/films/';
+request(URL, function (err, res, body) {
+  if (err) {
+    return console.log(err);
+  }
+  const objResults = JSON.parse(body).results;
+  let role = 0;
+  objResults.forEach(function (cast) {
+    cast.characters.forEach(function (character) {
+      if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+        role++;
+      }
+    });
+  });
+  console.log(role);
+});
