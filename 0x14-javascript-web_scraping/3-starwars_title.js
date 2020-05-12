53c9ef7fd4ac432571db9c707854b4b806760a09
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-12 17:39:55 +0000

    check 5 on task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,9 +5,9 @@ let object;
 
 request.get('https://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]), function (err, res, body) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     object = JSON.parse(body);
-    console.log(object.title);
+    console.log(object['title']);
   }
 });
