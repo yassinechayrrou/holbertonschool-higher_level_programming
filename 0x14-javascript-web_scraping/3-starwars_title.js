ec99c8affcc67376ae8cf1cf6e7deddb937a3a2d
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-12 18:07:00 +0000

    dddd

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
+const ep = parseInt(process.argv[2]);
 let object;
-
-request.get('https://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]), function (err, res, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    object = JSON.parse(body);
-    if (object.title === undefined) {
-      process.stdout.write('');
+if (ep < 8) {
+  const URL = 'https://swapi-api.hbtn.io/api/films/' + ep;
+  request.get(URL, function (err, res, body) {
+    if (err) {
+      console.log(err);
     } else {
-      console.log(object.title);
+      object = JSON.parse(body);
+      if (object.title === undefined) {
+        process.stdout.write('');
+      } else {
+        console.log(object.title);
+      }
     }
-  }
-});
+  });
+}
