6b7a10254910775cd455f58043d78538b9312a24
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-12 17:35:29 +0000

    Task 3: Star Wars movie title api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+let object;
+
+request.get('https://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]), function (err, res, body) {
   if (err) {
     console.error(err);
   } else {
-    const object = JSON.parse(body);
+    object = JSON.parse(body);
     console.log(object.title);
   }
 });
