e22e337f8e560da645a05078d54edbb448df1936
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-12 18:08:43 +0000

    dddd

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const ep = parseInt(process.argv[2]);
 let object;
-if (ep < 8) {
+if (ep < 7 && ep > 0) {
   const URL = 'https://swapi-api.hbtn.io/api/films/' + ep;
   request.get(URL, function (err, res, body) {
     if (err) {
