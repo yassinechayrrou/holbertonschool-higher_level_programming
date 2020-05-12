638c255b945d51c5313bfe10466b27cd24af0589
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-12 22:33:31 +0000

    Task 6: please check for me

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,15 +7,14 @@ request(URL, function (err, res, body) {
     return console.log(err);
   }
   const obj = JSON.parse(body);
-  let dict = {};
+  const dict = {};
   obj.forEach(function (objResults) {
-    if (objResults.completed === true ) {
-      let id = objResults.userId;
-      if ((id in dict) === false) {
-        dict.id = 0;
-      }
-      dict.id += 1;
+    if (dict[objResults.userId] === undefined) {
+      dict[objResults.userId] = 0;
     }
-  }); 
+    if (objResults.completed === true) {
+      dict[objResults.userId] += 1;
+    }
+  });
   console.log(dict);
 });
