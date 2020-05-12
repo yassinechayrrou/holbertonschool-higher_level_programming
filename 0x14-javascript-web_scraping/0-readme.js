65ecab960f80ffb61a4cef208c0357c5cd58cb99
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-12 16:29:17 +0000

    Task 0: readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,9 @@
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, readMe) {
-  if (readMe) {
+  if (err != null) {
+    console.log(err);
+  } else {
     process.stdout.write(readMe);
-  }
-  if (err) {
-    console.error(err);
   }
 });
