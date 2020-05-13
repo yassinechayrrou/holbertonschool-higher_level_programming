2cbb1feef983fa3e97f7157c44eb170e22ce5bb3
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-13 08:55:29 +0000

    Fixed red check output fail

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,16 @@
 const request = require('request');
 const URL = process.argv[2];
 request(URL, function (err, res, body) {
-  if (err) {
+  if (err != null) {
     return console.log(err);
   }
   const objResults = JSON.parse(body).results;
   let role = 0;
   objResults.forEach(function (cast) {
     cast.characters.forEach(function (character) {
-      if (character === 'https://swapi-api.hbtn.io/api/people/18/') {
+      const splitChar = character.split('/');
+      const len = splitChar.length;
+      if (splitChar[len - 2] === '18') {
         role++;
       }
     });
