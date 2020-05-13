f5041fd7b6bc70bfe6ba4f7234ab6d512f061110
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-13 09:34:22 +0000

    outputFail fixed condtion

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,10 +9,10 @@ request(URL, function (err, res, body) {
   const obj = JSON.parse(body);
   const dict = {};
   obj.forEach(function (objResults) {
-    if (dict[objResults.userId] === undefined) {
-      dict[objResults.userId] = 0;
-    }
     if (objResults.completed === true) {
+      if (dict[objResults.userId] === undefined) {
+        dict[objResults.userId] = 0;
+      }
       dict[objResults.userId] += 1;
     }
   });
