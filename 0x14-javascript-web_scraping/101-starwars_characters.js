d35ec71fb8b492a114758a4dc4380a4444daabb6
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-13 14:46:45 +0000

    Does not work yet

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,22 +2,23 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const URL = 'https://swapi-api.hbtn.io/api/films/';
-request(URL, function (err, res, body) {
-  if (err) {
-    return console.log(err);
-  }
-  const films = JSON.parse(body).results;
-  films.forEach(function (episode) {
-    if (episode.episode_id === parseInt(movieId)) {
-      episode.characters.forEach(function (character) {
-        request(character, function (err, res, body) {
-          if (err) {
-            return console.log(err);
-          }
-          console.log(JSON.parse(body).name);
-        });
-      });
-    }
-  });
+const URL = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(URL, (err, res, body) => {
+  if (err) { return console.log(err); }
+  const charactersURL = JSON.parse(body).characters;
+  recurr(charactersURL, 0);
 });
+
+function recurr(characters, idx) {
+  for (let i = 0; i < )
+  characters.forEach(character => {
+    request(character, (err, res, body) => {
+      if (err) { return console.log(err); }
+      console.log(JSON.parse(body).name);
+      if ( idx + 1 < character.length) {
+        recurr(character, idx + 1);
+      }
+    });
+  });
+}
