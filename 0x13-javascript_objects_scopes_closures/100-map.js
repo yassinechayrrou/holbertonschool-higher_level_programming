0ba2f2eb99eeb4a511df6d6517e808cd88193d55
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-07-05 20:28:01 +0100

    fixed occurent elements

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,9 @@
 
 const outer = require('./100-data.js');
 
-const inner = outer.list.map(x => outer.list.indexOf(x) * x);
+const inner = outer.list.map((x, index) => {
+  return x * index;
+});
 
 console.log(outer.list);
 console.log(inner);
