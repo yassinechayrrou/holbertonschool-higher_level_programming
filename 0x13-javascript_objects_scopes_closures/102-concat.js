13d66008591f48003b65be1eebb908ced4e41be7
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-07-06 00:08:06 +0100

    Semistandard

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 const first = fs.readFileSync(process.argv[2], 'utf8');
 const second = fs.readFileSync(process.argv[3], 'utf8');
-writeFileSync(process.argv[4], first + second, err => {
-  if (err) { console.log(err); }
+fs.writeFileSync(process.argv[4], first + second, err => {
+  if (err) { console.log(err);
+  }
 });
