4fad39d2123c6611fc8fe9b1df2d914fb7959947
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-03-01 14:16:02 +0100

    Advanced Task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""LockedClass module"""
+
 class LockedClass:
-    """LockedClass"""
+    """LockedClass a class that prevents any attributes other then assigned
+    in __slots__"""
 
     __slots__ = ['first_name']
